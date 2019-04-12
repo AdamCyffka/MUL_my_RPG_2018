efb34e3c938c213317f6f0b401a50959d8244f63
Valentin Masson <valipss@localhost.localdomain>
2019-04-12 16:18:19 +0200

    [+] quest stories completed

--- src/new_content2.c
+++ src/new_content2.c
@@ -8,18 +8,18 @@
 #include "rpg.h"
 #include "struct.h"
 
-game_quest_t new_quest(const char *quest_statement, bool _instant,
+/*game_quest_t new_quest(const char *quest_statement, const char *quest_dialog,
 int nb_of_task, int *rewards_id)
 {
     game_quest_t quest;
 
     quest.nb_of_task = nb_of_task;
     quest.progress = 0;
+    quest.quest_dialog = "Insert Dialog";
     quest.quest_statement = (char *) {quest_statement};
     quest.reward_id = rewards_id;
-    quest._instant = _instant;
     return (quest);
-}
+}*/
 
 game_inventory_t new_slot(void)
 {
