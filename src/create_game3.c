73ff0e89110fd45306bfbdd71a38a635d08da066
Maurin LIBERAL <mliberal@localhost.localdomain>
2019-04-30 15:11:40 +0200

    [+] Positions of ghost and change ghost's assests

--- src/create_game3.c
+++ src/create_game3.c
@@ -31,7 +31,7 @@ void create_settings(game_setting_t *settings)
     settings->last_time = 0;
     settings->delta_time = 0;
     settings->_paused = false;
-    settings->current = CAMP;
+    settings->current = FOREST;
     sfRenderWindow_setFramerateLimit(settings->window, 60);
     sfRenderWindow_setMouseCursorVisible(settings->window, sfFalse);
 }
