9d058f8db268835bc87e513a0827c768c63e7adf
Maurin LIBERAL <mliberal@localhost.localdomain>
2019-05-02 11:11:11 +0200

    [~] Correct minor bugs

--- src/lib_functions/my_libc_functions2.c
+++ src/lib_functions/my_libc_functions2.c
@@ -10,29 +10,29 @@
 
 void my_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
 void my_putstr(char *str)
 {
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0') {
-		my_putchar(str[i]);
-		i++;
-	}
+    while (str[i] != '\0') {
+        my_putchar(str[i]);
+        i++;
+    }
 }
 
 char *my_stradd(const char *str, char toadd)
 {
-	char *new_str = malloc(sizeof(char) * (my_strlen(str) + 2));
-	int tmp = 0;
+    char *new_str = malloc(sizeof(char) * (my_strlen(str) + 2));
+    int tmp = 0;
 
-	for (int tmp2 = 0; str[tmp2]; tmp2++) {
-		new_str[tmp] = str[tmp2];
-		tmp++;
-	}
-	new_str[tmp] = toadd;
-	new_str[tmp + 1] = '\0';
-	return (new_str);
+    for (int tmp2 = 0; str[tmp2]; tmp2++) {
+        new_str[tmp] = str[tmp2];
+        tmp++;
+    }
+    new_str[tmp] = toadd;
+    new_str[tmp + 1] = '\0';
+    return (new_str);
 }
