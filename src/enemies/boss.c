56f2d113cddf5c6ce1ddf7a4b13b72a553b88d4a
Maurin LIBERAL <mliberal@localhost.localdomain>
2019-05-05 20:23:21 +0200

    [~] Correct defeat scene in BOSS

--- src/enemies/boss.c
+++ src/enemies/boss.c
@@ -14,6 +14,7 @@ void teleport_player_boss(game_t *game)
     if (game->quests[BOSS_Q].state == Q_ACCEPTED &&
         game->settings->current != BOSS) {
         game->settings->current = BOSS;
+        game->quests[BOSS_Q].state = Q_IN_PROGRESS;
         game->stats->player.position.x = 1600;
         game->stats->player.position.y = 1600;
         sfSprite_setPosition(game->stats->player.sprite,
