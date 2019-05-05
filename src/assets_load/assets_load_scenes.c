3d31fd3c2cc0beda382139e11b9046e78bfbf76d
Valentin Masson <valipss@desktop-48oa7oh.home>
2019-05-05 19:27:39 +0200

    [-] coding style errors

--- src/assets_load/assets_load_scenes.c
+++ src/assets_load/assets_load_scenes.c
@@ -43,13 +43,17 @@ int assets_load_objs(game_object_t *objs, int nb)
 int assets_load_scenes(game_scene_t *scenes)
 {
     for (int tmp = MAIN_MENU; tmp <= DEFEAT; tmp++) {
-        if (assets_load_objs(scenes[tmp].objs, scenes[tmp].how_many[0]) == 84)
+        if (assets_load_objs(scenes[tmp].objs,
+        scenes[tmp].how_many[0]) == 84)
             return 84;
-        if (assets_load_sounds(scenes[tmp].sounds, scenes[tmp].how_many[1]) == 84)
+        if (assets_load_sounds(scenes[tmp].sounds,
+        scenes[tmp].how_many[1]) == 84)
             return 84;
-        if (assets_load_buttons(scenes[tmp].buttons, scenes[tmp].how_many[2]) == 84)
+        if (assets_load_buttons(scenes[tmp].buttons,
+        scenes[tmp].how_many[2]) == 84)
             return 84;
-        if (assets_load_texts(scenes[tmp].texts, scenes[tmp].how_many[3]) == 84)
+        if (assets_load_texts(scenes[tmp].texts,
+        scenes[tmp].how_many[3]) == 84)
             return 84;
     }
     return 0;
